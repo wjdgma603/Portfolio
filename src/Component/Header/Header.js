3862d6513635d6c4723157369feb48e9f235631f
wjdgma603 <144869990+wjdgma603@users.noreply.github.com>
2023-12-07 09:39:05 +0900

    Tailwind 변수 추가

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,12 @@
 import Info from "../../Common/Json/Info.json";
 const header = () => {
   return (
-    <header className="Header w-full h-[80px] flex justify-center items-center absolute top-0 left-0    ">
+    <header className="Header w-full h-[80px] flex justify-center items-center absolute top-0 left-0">
       <nav className="">
         <ul className="flex w-[600px] justify-between">
           {Info.map((InfoItem) => (
             <li>
-              <a className="text-[20px]" href="/">
+              <a className="text-[20px] text-black100" href="/">
                 {InfoItem.title}
               </a>
             </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#111",
+        black100: "#222",
+        black200: "#333",
+        black300: "#444",
+        black400: "#555",
+        white: "#fff",
+        white100: "#eee",
+        white200: "#ddd",
+        white300: "#ccc",
+        white400: "#bbb",
+      },
+    },
   },
   plugins: [],
 };
