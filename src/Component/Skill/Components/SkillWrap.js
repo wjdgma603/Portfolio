12d7d1c4da45392bb60672f53b28d0a7f41b1102
wjdgma603 <144869990+wjdgma603@users.noreply.github.com>
2023-12-12 17:49:27 +0900

    Skill SVG 넣기 구현

--- src/Component/Skill/Components/SkillWrap.js
+++ src/Component/Skill/Components/SkillWrap.js
@@ -1,9 +1,11 @@
 const SkillWrap = ({ Info }) => {
   return (
     <div className="flex flex-col items-center mt-[20px]">
-      <div className="rounded-[17.5px] w-[100px] h-[100px] bg-slate-500"></div>
+      <div
+        className={`rounded-[17.5px] w-[100px] h-[100px] bg-[#ffffff]`}
+        dangerouslySetInnerHTML={{ __html: Info.SvgPath }}></div>
       <div className="text-[18px] TextColor text-center whitespace-pre-wrap pt-[5px]">
-        {Object.values(Info)}
+        {Info.SkillName}
       </div>
     </div>
   );
