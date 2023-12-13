cbed46e9bceab3d81ae3fb82b7545d932420ea84
wjdgma603 <wjdgma603@naver.com>
2023-12-14 00:28:54 +0900

    Skill Section Front end Skill부분 정리

--- src/Component/Skill/Components/SkillWrap.js
+++ src/Component/Skill/Components/SkillWrap.js
@@ -1,8 +1,13 @@
 const SkillWrap = ({ Info }) => {
+  const Source = Info.BackgroundColor;
   return (
     <div className="flex flex-col items-center mt-[20px]">
       <div
-        className={`rounded-[17.5px] w-[100px] h-[100px] bg-[#ffffff]`}
+        className={`AppIcon`}
+        style={{
+          background: `${Info.BackgroundColor ? Info.BackgroundColor : "#fff"}`,
+          padding: `${Info.usePadding ? Info.usePadding : `15px`}`,
+        }}
         dangerouslySetInnerHTML={{ __html: Info.SvgPath }}></div>
       <div className="text-[18px] TextColor text-center whitespace-pre-wrap pt-[5px]">
         {Info.SkillName}
